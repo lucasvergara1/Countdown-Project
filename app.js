fbd03d1f5c5374c1168e37adedace6d910ef6f20
Lucas <lucas.vergara@live.com>
2024-02-21 22:46:47 -0300

    :construction: Create dates minutes and weekday

--- app.js
+++ app.js
@@ -21,3 +21,24 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4'); // nao teria problema selecionar as classes mas aqui mostra como esse método é bom e capaz
+
+let futureDate = new Date(2021, 1, 20, 1, 30,24);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();//essa é a maneira correta de fazer meses no JS
+month = months[month];
+//console.log(month)
+
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];//essa é a maneira correta de fazer dias no JS
+console.log(weekday);
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes} am`;
